198ce6c187dd709f69338cdd36818589ea9a19d6
dandresfsoto@gmail.com <dandresfsoto@gmail.com>
2020-04-04 22:31:51 -0500

    Add tests support

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,5 +1,6 @@
 from .base import *  # noqa
 from .base import env
+import sys
 
 DEBUG = True
 
@@ -31,3 +32,6 @@ DEBUG_TOOLBAR_CONFIG = {
 }
 
 INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
+
+if sys.argv[1] == 'test':
+    DATABASES = {"default": env.db("DATABASE_TEST_URL")}
